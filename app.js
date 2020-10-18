9371792a8a0a0864d44e54176f7dda57dfc0aaed
Behnam <learndlt5@gmail.com>
2020-10-18 11:37:51 +0330

    Done

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
 const body = document.querySelector("body");
 // place holder for qub
 let qub;
+// place holder for command functions
+let functions;
+// place holder to store commands list
+let commands = [];
+// list of random movement
+const r = ["right", "left", "up", "down"];
 // listen on DOM loaded
 document.addEventListener("DOMContentLoaded", () => {
   // create a qub and add it style and append it to body
@@ -17,6 +23,8 @@ document.addEventListener("DOMContentLoaded", () => {
   qub.style.top = "100px";
   qub.style.left = "150px";
   body.append(qub);
+  functions = document.createElement("div");
+  body.append(functions);
 });
 
 // listen on key down
@@ -24,54 +32,75 @@ document.addEventListener("keydown", (e) => {
   e.preventDefault();
   // store key code in a place holder
   let key = e.keyCode;
-  console.log(key);
-  // go left
+
   if (key === 37) {
-    goLeft();
-    // go top
+    addCommand("left");
   } else if (key === 38) {
-    goTop();
-    // go right
+    addCommand("up");
   } else if (key === 39) {
-    goRight();
-    // go down
+    addCommand("right");
   } else if (key === 40) {
-    goDown();
-    // change color 
+    addCommand("down");
   } else if (key === 67) {
     qub.style.backgroundColor = changeColor();
+  } else if (key === 82) {
+    let temp = movementArray[Math.floor(Math.random() * movementArray.length)];
+    addCommand(temp);
+  } else if (key === 13 || key === 32) {
+    mover();
   }
 });
-
-// go left
-function goLeft() {
-  let temp = qub.offsetLeft;
-  temp -= 50;
-  qub.style.left = temp + "px";
-}
-
-// go right
-function goRight() {
-  let temp = qub.offsetLeft;
-  temp += 50;
-  qub.style.left = temp + "px";
-}
-
-// go top
-function goTop() {
-  let temp = qub.offsetTop;
-  temp -= 50;
-  qub.style.top = temp + "px";
-}
-
-function goDown() {
-  let temp = qub.offsetTop;
-  temp += 50;
-  qub.style.top = temp + "px";
-}
 
 // change color
 function changeColor() {
   const randomColor = Math.random().toString(16).substr(-6);
   return "#" + randomColor;
+}
+// add to command array
+function addCommand(params) {
+  let span = document.createElement("span");
+  span.textContent = params;
+  span.style.color = "white";
+  span.style.padding = "10px";
+  span.style.border = "1px solid grey";
+  span.addEventListener("mouseover", () => {
+    span.style.backgroundColor = "red";
+  });
+  span.addEventListener("mouseout", () => {
+    span.style.backgroundColor = "";
+  });
+  span.addEventListener("click",function(){
+    let curIndex = commands.indexOf(this)
+    commands.splice(curIndex,1)
+    functions.removeChild(this)
+  })
+  functions.append(span);
+  commands.push(span);
+  console.log(commands);
+}
+
+// run commands when we click on enter or space
+function mover() {
+  if (commands.length > 0) {
+    let cur = qub.getBoundingClientRect();
+    let el = commands.shift();
+    let item = el.textContent.replace("+", "");
+    functions.removeChild(el);
+    qub.innerHTML = "Move" + item;
+    if (item == "left") {
+      qub.style.left = cur.left - cur.width + "px";
+    }
+    if (item == "right") {
+      qub.style.left = cur.left + cur.width + "px";
+    } 
+    if (item == "up") {
+      qub.style.top = cur.top - cur.height + "px";
+    }
+    if (item == "down") {
+      qub.style.top = cur.top + cur.height + "px";
+    }
+    setTimeout(mover, 300);
+  } else {
+    qub.innerHTML = "Set Path";
+  }
 }
